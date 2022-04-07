3ee1d8e03357fbcb92a1178e5dcad7d89462af92
Your Name <you@example.com>
2022-04-06 21:35:57 -0700

    Finished web automation project

--- healthbadge.py
+++ healthbadge.py
@@ -12,57 +12,57 @@ from email.mime.base import MIMEBase
 from email import encoders
 
 
+while True:
+    #Logging into student health center and taking screenshot
+    driver = webdriver.Chrome(executable_path="/Users/rahulganesh/Desktop/Projects/sitelogin/chromedriver")
+    url = 'https://studenthealth.ucsc.edu/?_gl=1*1k2r5wh*_ga*MTg2NjExMTE4NC4xNjQ5MTA3MjEx*_ga_BWJ4Z4Y66X*MTY0OTEwNzIxMC4xLjEuMTY0OTEwNzI1MS4w'
+    driver.get(url)
+    username = 'Student Gold ID -- the one you use to log into the portal'
+    pswd = 'Password for the portal'
+    fillname = driver.find_element(By.XPATH, '//*[@id="username"]').send_keys(username)
+    fillpswd = driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(pswd)
+    clickbtn = driver.find_element(By.XPATH, '/html/body/section/div/form/button').click()
+    driver.implicitly_wait(20)
+    dateofbirth = '07/18/2003'
+    filldate = driver.find_element(By.XPATH, '//*[@id="dtDOB"]').send_keys(dateofbirth)
+    clicknext = driver.find_element(By.XPATH, '//*[@id="cmdStandardProceed"]').click()
+    clickfinal = driver.find_element(By.XPATH, '//*[@id="ctl03"]/div[2]/a').click()
+    driver.fullscreen_window()
+    time.sleep(2)
+    call(["screencapture", "screenshot.jpg"])
+
+    #Now email myself the image
+    email_user = 'email'
+    email_password = 'passwd'
+    email_send = 'can match email_user for simplicity or also change if you need to'
+
+    subject = 'subject'
+
+    msg = MIMEMultipart()
+    msg['From'] = email_user
+    msg['To'] = email_send
+    msg['Subject'] = subject
+
+    body = 'Hi there, sending this email from Python!'
+    msg.attach(MIMEText(body,'plain'))
+
+    filename='screenshot.jpg'
+    attachment  =open(filename,'rb')
+
+    part = MIMEBase('application','octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition',"attachment; filename= "+filename)
+
+    msg.attach(part)
+    text = msg.as_string()
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login(email_user,email_password)
 
 
-driver = webdriver.Chrome(executable_path="/Users/rahulganesh/Desktop/Projects/sitelogin/chromedriver")
-url = 'https://studenthealth.ucsc.edu/?_gl=1*1k2r5wh*_ga*MTg2NjExMTE4NC4xNjQ5MTA3MjEx*_ga_BWJ4Z4Y66X*MTY0OTEwNzIxMC4xLjEuMTY0OTEwNzI1MS4w'
-driver.get(url)
-username = 'raganesh'
-pswd = 'Sashapwd123!'
-fillname = driver.find_element(By.XPATH, '//*[@id="username"]').send_keys(username)
-fillpswd = driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(pswd)
-clickbtn = driver.find_element(By.XPATH, '/html/body/section/div/form/button').click()
-driver.implicitly_wait(20)
-dateofbirth = '07/18/2003'
-filldate = driver.find_element(By.XPATH, '//*[@id="dtDOB"]').send_keys(dateofbirth)
-clicknext = driver.find_element(By.XPATH, '//*[@id="cmdStandardProceed"]').click()
-clickfinal = driver.find_element(By.XPATH, '//*[@id="ctl03"]/div[2]/a').click()
-#pyautogui.screenshot('/System/Applications/Photos.app/image1.png')
-driver.fullscreen_window()
-time.sleep(2)
-call(["screencapture", "screenshot.jpg"])
+    server.sendmail(email_user,email_send,text)
+    server.quit()
 
-#Now email myself the image
-email_user = 'badgehealth@gmail.com'
-email_password = 'green123!'
-email_send = 'badgehealth@gmail.com'
-
-subject = 'subject'
-
-msg = MIMEMultipart()
-msg['From'] = email_user
-msg['To'] = email_send
-msg['Subject'] = subject
-
-body = 'Hi there, sending this email from Python!'
-msg.attach(MIMEText(body,'plain'))
-
-filename='screenshot.jpg'
-attachment  =open(filename,'rb')
-
-part = MIMEBase('application','octet-stream')
-part.set_payload((attachment).read())
-encoders.encode_base64(part)
-part.add_header('Content-Disposition',"attachment; filename= "+filename)
-
-msg.attach(part)
-text = msg.as_string()
-server = smtplib.SMTP('smtp.gmail.com',587)
-server.starttls()
-server.login(email_user,email_password)
-
-
-server.sendmail(email_user,email_send,text)
-server.quit()
-
-#Script only needs to run every 24 hours, in the morning only
+    #Script only needs to run every 24 hours, in the morning only
+    time.sleep(86400)
--- sitelogin.py
+++ sitelogin.py
@@ -2,13 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(executable_path="/Users/rahulganesh/Desktop/Projects/sitelogin/chromedriver")
-url = 'https://canvas.ucsc.edu/courses/53169/assignments'
+url = 'https://canvas.ucsc.edu/courses/53169/announcements'
 driver.get(url)
-username = 'raganesh'
-pswd = 'Sashapwd123!'
+username = 'canvas login username'
+pswd = 'canvas login pswd'
 fillname = driver.find_element(By.XPATH, '//*[@id="username"]').send_keys(username)
 fillpswd = driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(pswd)
 clickbtn = driver.find_element(By.XPATH, '/html/body/section/div/form/button').click()
-assignments = driver.find_elements(By.ID, 'assignment_group_upcoming_assignments')
-for i in assignments:
-    print(i)
+assignments = driver.find_elements(By.CLASS_NAME, 'ic-item-row ic-announcement-row')
+print("Announcements: ", assignments, "Length: ", len(assignments))
